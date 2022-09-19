379509c4b4bbea590641dcac31abbc86438fb947
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-19 02:57:01 -0400

    added more buitin functions

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * check_cycle - checks if a linked list as a loopy cycle
+ * @list: pointer to the head of node
+ * Return: o if no cycle and 1 if otherwise
+ */
+
+int check_cycle(listint_t *list){
+    listint_t *p2
+}
