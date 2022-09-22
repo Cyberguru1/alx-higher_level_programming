9515a04325fdd9a3e4788442ac733e1f6e75ff9f
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-22 22:54:01 +0100

    introduction to python programming

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     i = 1
     args = len(argv)
 
-    print(f"{args} {'argument' if args == 1 else 'arguments'}", end = "")
-    print(f"{'.' if args == 0 else ':'}")
+    print(f"{args} {'argument' if args == 2 else 'arguments'}", end = "")
+    print(f"{'.' if args == 1 else ':'}")
     while (i <= args):
         print(f"{i}: {argv[i]}")
         i += 1
