3642ab81d59f548b473fc9c7792857c9757061ea
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 00:33:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    funtion that prints out he content of
+    a list to a string
+    """
+    for val in my_list:
+        print('{:d}'.format(val))
