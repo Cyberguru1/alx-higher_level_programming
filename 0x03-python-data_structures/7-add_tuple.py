855f3ca91e84048bd57e21c1dc8b322b4c907f99
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 01:52:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     A function that adds up
     two tuples
     """
+    tuple_b = (0,0) if len(tuple_b) == 0 else tuple_b
+    tuple_a = (0,0) if len(tuple_a) == 0 else tuple_a
     arg1 = 0 if len(tuple_a) == 1 else tuple_a[1]
     arg2 = 0 if len(tuple_b) == 1 else tuple_b[1]
     firs_arg = tuple_a[0] + tuple_b[0]
     sec_arg = arg1 + arg2
+
     return (firs_arg, sec_arg)
