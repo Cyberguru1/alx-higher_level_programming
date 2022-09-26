37e991183ce0f07054e315b3dacf379b31db01a3
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 01:13:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     """
     a function that prints a matrix of integers
     """
-    if matrix == []:
+    if matrix == [[]]:
         return
-        
+    
