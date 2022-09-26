eeedf5bf67c5c9c3bc9915f59afbae7da9a3015e
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 01:04:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     a function that returns modified version
     of a copied list
     """
-    copy_list = my_list
+    copy_list = my_list.copy()
     if idx < 0 or idx > (len(my_list) - 1):
         return copy_list
     else:
