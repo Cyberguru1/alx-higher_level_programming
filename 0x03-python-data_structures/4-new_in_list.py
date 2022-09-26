9c9d3a4733a5296c349b7f8ea4a7cf06ffb16ec7
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 01:00:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -9,3 +9,4 @@ def new_in_list(my_list, idx, element):
         return copy_list
     else:
         copy_list[idx] = element
+        return copy_list
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def no_c(my_string):
+    """
+    A function that returns a string without
+    the alphabet c
+    """
+    
