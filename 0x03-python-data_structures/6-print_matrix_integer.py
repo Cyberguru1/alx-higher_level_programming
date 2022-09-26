cf271c660a9e8c72100fe0af7d9bebb8c0c3d9bc
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-26 01:44:01 +0100

    introduction to python programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -9,6 +9,4 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(x):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]),end=' ')
-            if j == len(matrix[i]):
-                print('$')
         print('')
