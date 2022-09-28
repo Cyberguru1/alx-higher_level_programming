faa74fe4d3678a4cb137a66a44c076c82eca6986
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 14:09:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -7,8 +7,3 @@ def common_elements(set_1, set_2):
     """
     new_list = [i for i in set_1 if i in set_1 and i in set_2]
     return new_list
-
-set_1 = { "Python", "C", "Javascript" }
-set_2 = { "Bash", "C", "Ruby", "Perl" }
-c_set = common_elements(set_1, set_2)
-print(sorted(list(c_set)))
