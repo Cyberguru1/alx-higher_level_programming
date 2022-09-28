b5853bec3e99e463d4847099aad30da444a56b6b
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 13:40:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,3 +5,15 @@ def square_matrix_simple(matrix=[]):
     This function computes the square
     of all integers of a matrix
     """
+    sqr = lambda x : x**2
+    new_matrix = matrix
+    for i in (new_matrix):
+        for j in (i):
+            j = map(sqr, j)
+    return new_matrix
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+print(square_matrix_simple(matrix))
