10832b16b195bfa0909bf3b88e7dc9b72fdd49d8
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 15:06:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    """
-    A function that returns a set
-    of common elements in two
-    sets
-    """
-    f = lambda i :  i in set_2 
-    new_list = list(filter(f, set_1))
-    return new_list
+    return (set_1 & set_2)
