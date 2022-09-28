f9872ef90d91dd87a14b78c10845d97178febf49
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 15:05:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    """"
-    A function that stores 
-    an element that is different
-    between two sets
-    """
     return (set_1 ^ set_2)
