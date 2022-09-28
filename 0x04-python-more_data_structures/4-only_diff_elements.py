a1599380f42cf2d3c1878c3048602c3a4366b5a5
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 14:47:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    
+    """"
+    A function that stores 
+    an element that is different
+    between two sets
+    """
+    f = lambda i :  i not in set_2 
+    new_list = list(filter(f, set_1))
+    return new_list
