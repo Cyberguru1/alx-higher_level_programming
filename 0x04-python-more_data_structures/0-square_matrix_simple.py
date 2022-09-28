138e9b92b0da3b72a4c39ad2dd64b8e310b4bc86
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 15:07:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    """
-    This function computes the square
-    of all integers of a matrix
-    """
     new_matrix = matrix.copy()
     f = lambda x : x**2
     for i in range(len(matrix)):
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
     summed =  sum(set(my_list))
     return (summed)
