42d8dbdbaa10ab733a1451004eacff06c8e74fc3
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 14:19:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,4 +5,6 @@ def uniq_add(my_list=[]):
     A function that adds up 
     unique element in a list
     """
-    return (len(set(my_list)) -1)
+    summed =  sum(set(my_list))
+
+    return (summed)
