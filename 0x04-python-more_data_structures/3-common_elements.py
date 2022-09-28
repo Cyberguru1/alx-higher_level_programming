5541fc635099a3c3a383560516ca62991d996873
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 14:14:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -5,5 +5,5 @@ def common_elements(set_1, set_2):
     of common elements in two
     sets 
     """
-    new_list = [i for i in set_1 if i in set_1 and i in set_2]
+    new_list = i for i in set_1 if i in set_1 and i in set_2
     return new_list
