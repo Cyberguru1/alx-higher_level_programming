c22a82d525150cae65a43b60416c89effacc19c6
Cyberguru1 <hamzacypher34@gmail.com>
2022-09-28 15:45:01 +0100

    introduction to python programming

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = [list(map(lambda x: x**2, matrix[i]) for i in range(len(matrix)))]
+    new_matrix = [list(map(lambda x: x**2, matrix[i])) for i in range(len(matrix))]
     return (new_matrix)
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_map(matrix)
-print(new_matrix)
-print(matrix)
