082e266fbb3af31d4b72cb6947b8c4ae50ed0591
Cyberguru1 <hamzacypher34@gmail.com>
2022-10-11 00:54:01 +0100

    introduction to python programming

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,7 +2,7 @@
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in range(x):
-        if isinstance(int(my_list[i]), int):        
+        if isinstance(my_list[i], int):        
             try:
                 print("{:d}".format(my_list[i]), end="")
                 c += 1
