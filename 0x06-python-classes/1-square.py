9a5d83e281869e19192ae1df11c5abb0bc1b8eb1
Cyberguru1 <hamzacypher34@gmail.com>
2022-10-12 23:56:01 +0100

    introduction to python programming

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""1-square.py"""
+
+class Square:
+    """Defines as square"""
+
+    def __init__(self, size):
+        """
+        Creates an instance of square
+        Args:
+             size: size of the square
+        """
+        self.__size = size
