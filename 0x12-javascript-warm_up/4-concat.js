1def43e94c4629d03235adf12102ca6490adf75c
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-19 20:42:23 -0600

    update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 
-if (process.argv[2] && process.argv[3]) {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-} else {
-  console.log('No argument');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
