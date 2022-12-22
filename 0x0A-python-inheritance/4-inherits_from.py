e671af224f26bd254c47a7bb13138b9121ba69c2
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 07:54:33 -0600

    update

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -16,4 +16,4 @@ def inherits_from(obj, a_class):
             returns boolean
     '''
 
-    return  type(obj) != a_class and isinstance(obj, a_class)
+    return type(obj) != a_class and isinstance(obj, a_class)
