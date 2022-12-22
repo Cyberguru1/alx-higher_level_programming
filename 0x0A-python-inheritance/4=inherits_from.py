8765e098b87d33d24b7e43fd7892bd3022804b3b
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 07:46:00 -0600

    update

---
+++ 0x0A-python-inheritance/4=inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+        function: inherits_from(obj, a_class)
+
+        Args:
+            obj: object attribute to check
+            a_class: the class to compare with
+        Returns:
+            returns boolean
+    '''
+
+    return True if issubclass(a_class, ) else False
