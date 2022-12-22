ed147340389cc9d836ce39b0c21fb04a3e07cc4c
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 09:24:48 -0600

    update

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,8 +13,8 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.__size = size
-        super().__init__(self.__size, self.__size)
         Rectangle.integer_validator(self, 'size',size)
+        
 
     def __str__(self):
         """ print"""
