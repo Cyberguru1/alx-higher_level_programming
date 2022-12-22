252db02909d00fd747956facc7e4a339d03d32fe
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 07:38:52 -0600

    update

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -12,4 +12,6 @@ def is_kind_of_class(obj, a_class):
         Args:
             obj: object attribute to check
             a_class: the class to compare with
+        Returns:
+            returns boolean
     '''
