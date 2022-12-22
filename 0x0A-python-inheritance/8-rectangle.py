f87ca94200013f639b2b847141f96dafce145f12
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 08:28:41 -0600

    update

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -8,6 +8,7 @@ BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 class Rectangle(BaseGeometry):
     '''class: Rectangle inherited from baseGeometry
     '''
+    self.integer_balida
 
     
     def __init__(self, width, height) -> None:
