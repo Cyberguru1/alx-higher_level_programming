14c8be0570f8fe6b76b5870c1020efddaee20d0c
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 07:10:13 -0600

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,9 +4,7 @@ function that return a list of objects
 '''
 
 def lookup(obj):
-	""" function: lookup()
-	Returns list of available
-	attributes and methods of an object
-
-	"""
+	''' function: lookup()
+	Returns a list of available
+	'''
 	return dir(obj)
