062678377e59e197c4ca0b759420ca53970e9955
Cyberguru1 <hamzacypher34@gmail.com>
2022-12-22 07:43:41 -0600

    update

---
+++ 0x0A-python-inheritance/4=inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''function that returns True if the object is an instance of,
+    or if the object is an instance of a class that inherited from,
+    the specified class ; otherwise False
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+        function: is_kind_of_class(obj, a_class)
+
+        Args:
+            obj: object attribute to check
+            a_class: the class to compare with
+        Returns:
+            returns boolean
+    '''
+
+    return True if isinstance(obj, a_class) else False
