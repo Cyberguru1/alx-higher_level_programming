4ba4b7f09eef984b4d5c1e4e4f89083f7210ee31
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-02 02:38:58 -0600

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -13,17 +13,16 @@ class Student:
             age (int): The student agen number
         """
         self.first_name = first_name
-        self.last_name  = last_name
+        self.last_name = last_name
         self.age = age
-
-
+    
 
     def to_json(self, attrs=None):
         """ Retrives the dictionary representation of the student
         Args:
             Attrs (list): (optional)
-            
         """
+
         if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
@@ -33,5 +32,5 @@ class Student:
         Args:
             json (dict): The key/value pairs to replace attributes with.
         """
-        for k,v in json.items():
+        for k, v in json.items():
             setattr(self, k, v)
