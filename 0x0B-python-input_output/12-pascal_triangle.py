6f053d940402d99c8fd9738be19c29cb6c1b9ec4
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-02 02:32:41 -0600

    update

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Implementation of pascal triangle"""
+
+def pascal_triangle(n):
+    """implements the pascal traingle
+    Args:
+        n (int): number of terms of pascal triangle
+    """
+    terms = []
+    if n <= 0:
+        return terms
+    for i in range(n):
+        term = []
+        for j in range(i+1):
+            value = fact(i) // (fact(j)* fact(i - j))
+            term.append(value)
+        terms.append(term)
+    return terms
+
+def fact(n):
+    """implements factorial
+    Args:
+        n(int) : number to find factorial
+    Returns:
+        returns the factorial
+    """
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
