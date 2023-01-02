dc557caf806c7f3ae5dc823fc918d0d2c54e17af
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-02 02:40:00 -0600

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,7 +15,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
 
     def to_json(self, attrs=None):
         """ Retrives the dictionary representation of the student
