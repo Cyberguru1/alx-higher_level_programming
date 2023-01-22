01af1ebfcdfa6ccd0e7999596b71906b0c1e9c50
Cyberguru1 <hamzacypher34@gmail.com>
2023-01-22 17:49:57 -0600

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 # a bash script that returns the size of the body response
 
-out=$(curl -sI "$1" | grep content-length)
-
+out=$(curl -sI "$1"| grep -i content-length | awk ' { printf $2 }')
 echo $out
