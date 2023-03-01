c001a217f4797050ce4da78606f837a8671319c4
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-01 02:43:59 -0600

    update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const files = require('fs');
 files.readFile(process.argv[2], 'utf8', function (error, content) {
-    console.log(error || content);
+  console.log(error || content);
 });
