1306ea6ea5fb33e06b35cf831885e36c1f86db00
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-01 03:15:45 -0600

    update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,14 +2,14 @@
 
 const req = require('request');
 const epNum = process.argv[2];
-const api_url = 'https://swapi-api.alx-tools.com/api/films/';
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/';
 
-req(api_url + epNum, function (err, response, body) {
+req(API_URL + epNum, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200){
+  } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
-    console.log(responseJSON['title']);
+    console.log(responseJSON.title);
   } else {
     console.log('Error code: ' + response.statusCode);
   }
