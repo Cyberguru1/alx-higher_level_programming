968a0693243fa3db31fddcb61df454ee1d0fb8c7
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-01 03:05:26 -0600

    update

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const req = require('request');
+const epNum = process.argv[2];
+const api_url = 'https://swapi-api.hbtn.io/api/films';
+
+req(api_url + epNum, function (err, response, body) {
+    if (err) {
+        console.log(err);
+    } else if (response.statusCode === 200){
+        const responseJSON = JSON.parse(body);
+        console.log(responseJSON.title);
+    } else {
+        console.log('Error code: ' + response.statusCode);
+    }
+});
