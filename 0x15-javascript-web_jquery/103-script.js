94d835cb0a45f3ac42381511deeb64d856f505df
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-05 16:31:30 -0500

    more writeup

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// hamza saidu
+// function to change color, when script is imported from 
+// head tag <head>
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+    
+// });
+
+$(function() {
+    $("#btn_translate").onclick(function(){
+        var lang = $("#language_code").val();
+        var url = `https://hellosalut.stefanbohacek.dev/?lang=${lang}`;
+        $.get(url, function(data, status){
+            $("#hello").text(data.hello);
+        });
+    });
+
+})
