41bbe3d7f07e16a7cbabe050447d046648abcb59
Cyberguru1 <hamzacypher34@gmail.com>
2023-06-05 11:30:17 -0500

    more writeup

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 // using query selectors with DOM manipulation
-
+$(function()
+{
+    $("header").css("color", "#FF0000")
+});
---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// click function 
+
+$(function()
+{
+    $("#red_header").click(function(){$("header").css("color", "#FF0000")});
+})
